dca41147254cc9f62ea2acc4fef655cc797cd578
Xebros85 <rorban1@live.com>
2024-10-21 16:15:14 -0500

    Added node class in lib/node_class.rb

--- lib/node_class.rb
+++ lib/node_class.rb
@@ -3,4 +3,10 @@
 
 class Node
   # contains two instance variables, #value and #next_node, set both as nil by default
+  attr_accessor :data, :next_node
+  
+  def initialize(data, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
 end
